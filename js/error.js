5b5a2d79a0dda5a640f4cf4f2556bf34d05ce9cb
Daniela Valdivia <danielavaldivia.dev@gmail.com>
2026-02-10 19:15:44 -0500

    feat:change names from spanish to english

--- js/error.js
+++ js/error.js
@@ -2,29 +2,29 @@ document.addEventListener("DOMContentLoaded", function(){
     const inputEmail = document.querySelector('#email')
     const inputGitHub= document.querySelector('#github')
 
-    inputEmail.addEventListener('blur', validar);
-    inputGitHub.addEventListener('blur', validar);
+    inputEmail.addEventListener('blur', validateInput);
+    inputGitHub.addEventListener('blur', validateInput);
 
 
 
-    function validar(e){
+    function validateInput(e){
         if(e.target.value.trim() === ''){
-            mostrarAlerta(`Please enter a valid ${e.target.id} address.`, e.target.parentElement);
+            showInputAlert(`Please enter a valid ${e.target.id} address.`, e.target.parentElement);
             return;
         } 
 
-        if(e.target.id === 'email' && !validarEmail(e.target.value)){
-            mostrarAlerta(e.target.parentElement);
+        if(e.target.id === 'email' && !validateEmail(e.target.value)){
+            showInputAlert(e.target.parentElement);
             return;
         } 
 
-        limpiarAlerta(e.target.parentElement);
+        clearInputAlert(e.target.parentElement);
     }
 
-    function mostrarAlerta(mensaje, referencia){
-        limpiarAlerta(referencia);
+    function showInputAlert(errorMessage, reference){
+        clearInputAlert(reference);
 
-        const image = document.createElement('SPAN');
+        const image = document.createElement('span');
         image.classList.add('important-icon');
 
         fetch('./assets/images/important-icon.svg')
@@ -43,32 +43,32 @@ document.addEventListener("DOMContentLoaded", function(){
         });
 
 
-        const error = document.createElement('P');
-        error.textContent =mensaje;
+        const error = document.createElement('p');
+        error.textContent = errorMessage;
         error.classList ='form-field__hint'
         error.style.color ='var(--orange500)'
 
         
-        const message = document.createElement('DIV');
+        const message = document.createElement('div');
         message.classList='form-field__message';
         message.appendChild(image);
         message.appendChild(error);
         message.classList.add('input-alert');
 
-        referencia.appendChild(message);
+        reference.appendChild(message);
     }
 
 
-    function limpiarAlerta(referencia) {
-        const alerta = referencia.querySelector('.input-alert');
-        if(alerta){
-            alerta.remove();
+    function clearInputAlert(reference) {
+        const alert = reference.querySelector('.input-alert');
+        if(alert){
+            alert.remove();
         }
     }
 
-    function validarEmail(email){
+    function validateEmail(email){
         const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
-        const resultado = regex.test(email)
-        return resultado;
+        const result = regex.test(email)
+        return result;
     }
 });
