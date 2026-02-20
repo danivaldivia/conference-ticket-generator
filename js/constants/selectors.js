801bc9710b1dc0cbe240a47a244ec7a17581a38a
Daniela Valdivia <danielavaldivia.dev@gmail.com>
2026-02-20 17:32:15 -0500

    feat: create upload-status and add remove image (avatar)

--- js/constants/selectors.js
+++ js/constants/selectors.js
@@ -11,3 +11,9 @@ export const originalMessageColor = getComputedStyle(messageAvatar).color;
 
 export const previewSvg = document.querySelector("#preview-svg");
 export const previewImg = document.querySelector("#preview-img");
+
+export const uploadStatus = document.querySelector(".file-upload__status");
+export const uploadDrag = document.querySelector(".file-upload__drag-text");
+
+export const removeBtn = document.querySelector("#remove-btn");
+export const changeBtn = document.querySelector("#change-btn");
--- js/modules/preview.js
+++ js/modules/preview.js
@@ -8,6 +8,9 @@ import {
   previewImg,
   svgAlert,
   messageAvatar,
+  uploadDrag,
+  uploadStatus,
+  removeBtn,
 } from "../constants/selectors.js";
 
 function sizeAlert(strokeColor, alertMessage, alertColor) {
@@ -21,41 +24,56 @@ function sizeAlert(strokeColor, alertMessage, alertColor) {
   });
 }
 
+function removeAvatar() {
+  inputAvatar.value = "";
+  previewImg.src = "";
+  previewImg.hidden = true;
+  previewSvg.hidden = false;
+  uploadStatus.hidden = true;
+  uploadDrag.hidden = false;
+}
+
+function validPreview(e) {
+  const file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  if (!file.type.startsWith("image/")) {
+    return;
+  }
+
+  const fileSize = file.size;
+  const fileType = file.type;
+
+  if (file) {
+    console.log(`Peso: ${(fileSize / 1024).toFixed(2)} KB`);
+    console.log(`Formato: ${fileType}`);
+  }
+
+  if (fileSize > maxSize) {
+    sizeAlert(
+      "var(--orange-500)",
+      "File too large. Please upload a photo under 500KB.",
+      "var(--orange-500)",
+    );
+  } else {
+    sizeAlert("currentColor", originalMessageText, originalMessageColor);
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      previewImg.src = reader.result;
+      previewImg.hidden = false;
+      previewSvg.hidden = true;
+
+      uploadStatus.hidden = false;
+      uploadDrag.hidden = true;
+    };
+
+    reader.readAsDataURL(file);
+  }
+}
+
 export function initPreview() {
-  inputAvatar.addEventListener("change", (e) => {
-    const file = e.target.files[0];
-    if (!file) {
-      return;
-    }
-    if (!file.type.startsWith("image/")) {
-      return;
-    }
-
-    const fileSize = file.size;
-    const fileType = file.type;
-
-    if (file) {
-      console.log(`Peso: ${(fileSize / 1024).toFixed(2)} KB`);
-      console.log(`Formato: ${fileType}`);
-    }
-
-    if (fileSize > maxSize) {
-      sizeAlert(
-        "var(--orange-500)",
-        "File too large. Please upload a photo under 500KB.",
-        "var(--orange-500)",
-      );
-    } else {
-      sizeAlert("currentColor", originalMessageText, originalMessageColor);
-      const reader = new FileReader();
-
-      reader.onload = () => {
-        previewImg.src = reader.result;
-        previewImg.hidden = false;
-        previewSvg.hidden = true;
-      };
-
-      reader.readAsDataURL(file);
-    }
-  });
+  inputAvatar.addEventListener("change", validPreview);
+  removeBtn.addEventListener("click", removeAvatar);
 }
