0fe9d9812a5827f9e764d06f9409b5f1ae5655bf
Daniela Valdivia <danielavaldivia.dev@gmail.com>
2026-03-26 14:36:16 -0500

    feat: add styles for brand-header and intro section, and add svg ticket

--- src/feature/ticket/ticket-page.js
+++ src/feature/ticket/ticket-page.js
@@ -2,10 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const data = new URLSearchParams(window.location.search);
   const userName = data.get("fullname");
   const userEmail = data.get("email");
-  const userGitHub = data.get("github");
 
-  document.querySelector(".intro__title").textContent =
-    `Congrats, ${userName}! Your ticket is ready.`;
-  document.querySelector(".intro__description").textContent =
-    `We've emailed your ticket to ${userEmail}.com and will send updates in the run up to the event.`;
+  document.querySelector(".intro").innerHTML = `
+    <h1 class="intro__title">Congrats, <span class="intro__title--effect">${userName}!</span> Your ticket is ready.</h1>
+    <p class="intro__description">We've emailed your ticket to <span class="intro__title--highlight">${userEmail}</span> and will send updates in the run up to the event.</p>
+    `;
 });
