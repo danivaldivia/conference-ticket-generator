3c981a07a78631424a402ce098a80d0530549b73
Daniela Valdivia <danielavaldivia.dev@gmail.com>
2026-04-18 18:00:39 -0500

    fix: correct the dimensions of the avatar photo and alert message

--- src/feature/registration/constants/file-form.js
+++ src/feature/registration/constants/file-form.js
@@ -1,2 +1,3 @@
-export const MAX_FILE_SIZE = 200 * 1024;
+export const MIN_FILE_SIZE = 200 * 1024;
+export const MAX_FILE_SIZE = 500 * 1024;
 export const ALLOWED_FILES_TYPES = ["image/png", "image/jpeg"];
